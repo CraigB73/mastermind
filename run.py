610ecf8264dea7f2ebbf1707869fe6fd5abadbd1
CraigB73 <craigbritton73@gmail.com>
2023-12-02 17:15:38 +0100

    Add instruction to game.

--- run.py
+++ run.py
@@ -3,3 +3,11 @@ import random
 CODE_LIST = ['Z', 'X', 'C', 'V', 'B']
 CODE = []
 PLAYER_GUESS = []
+print()
+print("You will be given 10 tries.\n")
+print("A max of 5 letters to choose from.")
+print("Letters are located on the last row of the keyboard starting with: z, x, c, v, and b.\n")
+print('Your entries are not case sensitive')
+print('When entering you code make sure you have no empty spaces after the comma ex:(y,y, , )\n')
+print('Crack the code before you run out of tries to become a "Mastermind."\n\n')
+print("To start the game. Enter your difficulty: Easy, Normal, or Expert\n")
