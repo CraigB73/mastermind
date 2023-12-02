8bdb09092ecd6b96092977db272adaf6c101385c
CraigB73 <craigbritton73@gmail.com>
2023-12-02 17:13:46 +0100

    Intial commit

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+CODE_LIST = ['Z', 'X', 'C', 'V', 'B']
+CODE = []
+PLAYER_GUESS = []
