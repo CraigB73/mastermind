d7a3f10ce37504b32fb83fc698c9b752724c5e3c
CraigB73 <craigbritton73@gmail.com>
2023-12-02 18:07:35 +0100

    Add random_generated_code().

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import random
 
 CODE_LIST = ['Z', 'X', 'C', 'V', 'B']
-CODE = []
 PLAYER_GUESS = []
+
 print()
 print("You will be given 10 tries.\n")
 print("A max of 5 letters to choose from.")
@@ -23,11 +23,33 @@ def get_difficulty_input():
         try:
             if player_input.lower() in level_values:
                 
-                return player_input.capitalize()
+                return player_input.lower()
             else: 
                 raise ValueError(f'Make sure to enter: easy, normal, or expert.\n')    
         except ValueError as e:
             print(e)
+
+def random_generated_code(difficulty, code_charters):
+    """
+    Generates random game code depending difficulty input by player.
+    Checks the value of difficulty input inorder to create random game code.   
+    """
+    
+    generated_code = [random.choice(code_charters) for _ in range(len(code_charters))] # Generates a random code from
+    
+    try:
+        if difficulty == 'easy':
+            return generated_code[: len(generated_code) - 2]
+        elif difficulty == 'normal':
+            return generated_code[: len(generated_code) - 1]
+        elif difficulty == 'expert':
+            return generated_code
+        else: 
+            raise ValueError("Invalid difficulty level. Check that you have entered correct value")
+    except ValueError as e:
+        print(e)
+
                     
-level_choice = get_difficulty_input()
-print(level_choice)  
+difficulty = get_difficulty_input()
+code = random_generated_code(difficulty, CODE_LIST)
+print(difficulty, code)  
