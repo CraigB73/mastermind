d4da8594f8466dbf538fcce47a4dbf3d9ab37eb2
CraigB73 <craigbritton73@gmail.com>
2023-12-03 22:35:34 +0100

    Add guess_input_validation. Changed some naming in player_guess_check

--- run.py
+++ run.py
@@ -21,21 +21,21 @@ def get_difficulty_input():
         player_input = str(input(f"Enter you difficulty level: \n"))
         try:
             if player_input.lower() in level_values:
-                
+
                 return player_input.lower()
-            else: 
-                raise ValueError(f'Make sure to enter: easy, normal, or expert.\n')    
+            else:
+                raise ValueError(f'Make sure to enter: easy, normal, or expert.\n')
         except ValueError as e:
             print(e)
 
 def secret_generated_code(difficulty, code_charters):
     """
     Generates random secret game code depending difficulty input by player.
-    Checks the value of difficulty input inorder to create random game code.   
+    Checks the value of difficulty input inorder to create random game code.
     """
-    
+
     generated_code = [random.choice(code_charters) for _ in range(len(code_charters))] # Generates a random code from
-    
+
     try:
         if difficulty == 'easy':
             return generated_code[: len(generated_code) - 2]
@@ -43,63 +43,110 @@ def secret_generated_code(difficulty, code_charters):
             return generated_code[: len(generated_code) - 1]
         elif difficulty == 'expert':
             return generated_code
-        else: 
+        else:
             raise ValueError("Invalid difficulty level. Check that you have entered correct value")
     except ValueError as e:
         print(e)
-        
+
 player_guess = []
 def get_player_guess():
     """
     Gets players guess towards the hidden code.
     """
-    global player_guess     
+    global player_guess
     player_input = str(input("Enter your guess here: ").upper())
     string_list = player_input.split(",")
     player_guess.extend(string_list)
-    return player_guess
+    
+    if guess_input_validation(string_list, difficulty, CODE_LIST):
+        return player_guess
 
+def guess_input_validation(players_guess, difficulty, code_charaters):
+    """
+    Validates player input against computers generate game code.
+    Validates player has entered only the lettes available to the game.
+    Validates that the list length is equal to chosen diffculty.
+    """
+    Message = {
+        "easy": 3,
+        "normal": 4,
+        "expert": 5
+    }
 
-   
-def guess_calculate(random_secret_code, player_guess):
+    guess = players_guess
 
+    for msg in Message: # Loops over the Message dictionary 
+        if difficulty == msg:
+            number = Message[msg]
+            try:
+                for char in guess:
+                    if char not in code_charaters:
+                        raise TypeError
+            except TypeError as e:
+                print(f'{e} You can only enter the letters: Z, X, C, V, and B!\n')
+                return False, get_player_guess()
+            try:
+                if len(guess) != int(number):
+                    raise ValueError
+            except ValueError as e:
+                print(f'{e} For your code to work you need to enter {number} code letters!')
+                return False, get_player_guess()
+            return True
+        
+def player_guess_check(random_secret_code, player_guess):
+    """
+    Checks player's guess against the computer secret code.
+    Updates the number of correct letters in the correct postion.
+    Updates the number oc correct letters of the player's guess.
+    """
     secret_code_counter = Counter(random_secret_code)
     player_guess_counter = Counter(player_guess)
-    
+
     common_values = secret_code_counter & player_guess_counter
-     
-    total_values = sum(common_values.values())
-    
-    zip_iterator = zip(random_secret_code, player_guess) 
-    correct_posititons = sum(1 for sc, pg in zip_iterator if sc == pg)
+    # total_values = sum(common_values.values())
+
+
+    zip_iterator = zip(random_secret_code, player_guess)
+    # Gets total of compared values in an object of tuples if they are of equal value.
+    correct_posititons = sum(1 for secret, guess in zip_iterator if secret == guess)
+
+    #Gets the minimum values between sec_code_counter and player_guess_counter values.
     correct_values = sum(min(secret_code_counter[values], player_guess_counter[values]) for values in common_values)
 
-    common_values = total_values - correct_posititons
-    
-    message = (f'Number of correct placements {correct_posititons}', f'Number of correct letters {correct_values}' )
+    #Subtracts
+    # common_values = total_values - correct_posititons
+
+    message = (f'Number of correct placements {correct_posititons}\n', f'Number of correct letters {correct_values}' )
     (a, b) = message
     print(a, b)
-    
-    
-random_game_code = []                
+
+
+
+# Define a variable to store the previous feedback
+previous_totals = None
+
+def check_guess_gamecode(player_code):
+    global previous_totals
+
+    # Calculate the feedback for the current player guess
+    feedback = player_guess_check(random_game_code, player_code)
+
+    # If there is previous feedback, print it
+    if previous_totals is not None:
+        print("PREVIOUS FEEDBACK:", previous_totals)
+
+    # Print the current feedback
+    print("CURRENT FEEDBACK:", feedback)
+
+    # Update the previous_feedback for the next iteration
+    previous_totals= feedback
+
+random_game_code = []
 difficulty = get_difficulty_input()
 code = secret_generated_code(difficulty, CODE_LIST)
 if code:
     random_game_code.extend(code)
-    print(code) 
+    print(code)
 get_player_guess()
-print(difficulty)
-
+check_guess_gamecode(player_guess)
 print(f'randomCode:  {random_game_code}\n', f'playerGuess: {player_guess}')
-guess_calculate(random_game_code, player_guess)
-
-#  game_code = Counter(random_code)
-#     player_guess = Counter(player_code)
-    
-#     compare_code = game_code & player_guess
-    
-#     total = sum(compare_code.values())
-
-    
-#     if total > 0:
-#         print("TOTAL", total)
